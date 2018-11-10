0754dfe4f885173a2bdf78937f1f1107de78e9a8
Unknown <skasbzhezinski@gmail.com>
2018-11-10 22:59:43 +0400

    Устанавливаю фокус в поле 'имя'

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 var link = document.querySelector(".button-link");
 var popup = document.querySelector(".modal-popup");
 var close = document.querySelector(".close-button");
+var userName = popup.querySelector("[name = name]"); // находим поле с атрибутом name = "name"
+var form = popup.querySelector("form"); // находим форму по тегу
 
 link.addEventListener("click", function(evt) {evt.preventDefault();
     popup.classList.add("modal-show");
+    userName.focus(); //устанавливаем фокус в поле с атрибутом name = "name"
 });
 close.addEventListener("click", function(evt) {evt.preventDefault();
-    popup.classList.remove("modal-show")
+    popup.classList.remove("modal-show");
 });
+form.addEventListener("submit", function(evt) { //ловим событие отправки формы
+    evt.preventDefault(); // отменяем действие по умолчанию
+    console.log("Отправляем форму");
+})
