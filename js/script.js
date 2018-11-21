f6f4534d656e00bc8a70e990158493a96e59f571
Юрий Скасырский <skasbzhezinski@gmail.com>
2018-11-21 18:56:11 +0400

    Exit the window by clicking Esc-button

--- js/script.js
+++ js/script.js
@@ -2,16 +2,46 @@ var link = document.querySelector(".button-link");
 var popup = document.querySelector(".modal-popup");
 var close = document.querySelector(".close-button");
 var userName = popup.querySelector("[name = name]"); // находим поле с атрибутом name = "name"
+var userEmail = popup.querySelector("[name = mail]"); // находим поле с атрибутом name = "email"
 var form = popup.querySelector("form"); // находим форму по тегу
+var mailStorage = localStorage.getItem("email"); //записываем в переменную значение введенного value
+var nameStorage = localStorage.getItem("name");
+
 
 link.addEventListener("click", function(evt) {evt.preventDefault();
-    popup.classList.add("modal-show");
+    popup.classList.add("modal-show"); //показывает всплывающее окно
     userName.focus(); //устанавливаем фокус в поле с атрибутом name = "name"
+    if (mailStorage) {
+        userEmail.value = mailStorage;
+    }
+    if (nameStorage) {
+        userName.value = nameStorage;
+    }
 });
+
 close.addEventListener("click", function(evt) {evt.preventDefault();
     popup.classList.remove("modal-show");
 });
+
 form.addEventListener("submit", function(evt) { //ловим событие отправки формы
     evt.preventDefault(); // отменяем действие по умолчанию
     console.log("Отправляем форму");
-})
+    if (!userName.value || !userEmail.value) { //если не введено хотя бы одно значение, выведем напоминание 
+        console.log("Нужно ввести Ваше имя и email");
+    } else {
+        localStorage.setItem("email", userEmail.value);
+        localStorage.setItem("name", userName.value); //иначе запишем введенные значения в localStorage
+    }
+    console.log(userName.value); //выводим в консоль введенные в поля значения
+    console.log(userEmail.value);
+});
+
+//выход из всплывающего окна по нажатию ESC
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
+    }
+});
