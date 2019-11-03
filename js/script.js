8b3a90bf22c9b92459a57082f9d77058b380e784
Юрий Скасырский <skasbzhezinski@gmail.com>
2019-11-03 12:08:13 +0300

    Сделал у попапа top: 100px; В script.js добавил вывод в консоль текста юзера. Добавил файл without-prefixes-style.css

--- js/script.js
+++ js/script.js
@@ -1,63 +1,67 @@
-var link = document.querySelector(".button-link");
+var link = document.querySelector(".button-link"); //???
 var popup = document.querySelector(".write-us");
 var close = document.querySelector(".close-button");
 var userName = popup.querySelector("[name = name]"); // находим поле с атрибутом name = "name"
 var userEmail = popup.querySelector("[name = mail]"); // находим поле с атрибутом name = "email"
+var userText = popup.querySelector("[name = text]"); // находим поле для ввода текста
 var form = popup.querySelector("form"); // находим форму по тегу
 var mailStorage = localStorage.getItem("email"); //записываем в переменную значение введенного value
 var nameStorage = localStorage.getItem("name");
 
 
-link.addEventListener("click", function(evt) {evt.preventDefault();
-    popup.classList.add("modal-show"); //показывает всплывающее окно
-    userName.focus(); //устанавливаем фокус в поле с атрибутом name = "name"
-    if (mailStorage) {
-        userEmail.value = mailStorage;
-    }
-    if (nameStorage) {
-        userName.value = nameStorage;
-    }
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show"); //показывает всплывающее окно
+  userName.focus(); //устанавливаем фокус в поле с атрибутом name = "name"
+  if (mailStorage) {
+    userEmail.value = mailStorage;
+  }
+  if (nameStorage) {
+    userName.value = nameStorage;
+  }
 });
 
-close.addEventListener("click", function(evt) {evt.preventDefault();
-    popup.classList.remove("modal-show");
-    popup.classList.remove("modal-error");
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
 
 //если localStorage не работает локально
 var isStorageSupport = true;
 
 try {
-    (mailStorage = localStorage.getItem("email")) ||
+  (mailStorage = localStorage.getItem("email")) ||
     (nameStorage = localStorage.getItem("name"));
 } catch (err) {
-    isStorageSupport = false;
+  isStorageSupport = false;
 }
 
 
-form.addEventListener("submit", function(evt) { //ловим событие отправки формы
-    evt.preventDefault(); // отменяем действие по умолчанию
-    console.log("Отправляем форму");
-    if (!userName.value || !userEmail.value) { //если не введено хотя бы одно значение, выведем напоминание 
-        console.log("Нужно ввести Ваше имя и email");
-        popup.classList.add("modal-error"); //добавляем класс в случае ошибочной отправки формы
-    } else {
-        if (isStorageSupport) { //условие: если localStorage работает
-            localStorage.setItem("email", userEmail.value);
-            localStorage.setItem("name", userName.value); //иначе запишем введенные значения в localStorage
-        }
-        console.log(userName.value); //выводим в консоль введенные в поля значения
-        console.log(userEmail.value);
+form.addEventListener("submit", function (evt) { //ловим событие отправки формы
+  evt.preventDefault(); // отменяем действие по умолчанию
+  console.log("Отправляем форму");
+  if (!userName.value || !userEmail.value) { //если не введено хотя бы одно значение, выведем напоминание
+    console.log("Нужно ввести Ваше имя и email");
+    popup.classList.add("modal-error"); //добавляем класс в случае ошибочной отправки формы
+  } else {
+    if (isStorageSupport) { //условие: если localStorage работает
+      localStorage.setItem("email", userEmail.value);
+      localStorage.setItem("name", userName.value); //иначе запишем введенные значения в localStorage
     }
+    console.log(userName.value); //выводим в консоль введенные в поля значения
+    console.log(userEmail.value);
+    console.log(userText.value);
+  }
 });
 
 //выход из всплывающего окна по нажатию ESC
-window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-        evt.preventDefault();
-        popup.classList.remove("modal-error");
-        if (popup.classList.contains("modal-show")) {
-            popup.classList.remove("modal-show");
-        }
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
     }
+  }
 });
